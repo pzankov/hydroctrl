bc42a42bbd77e225912ba51bec1e61569189a49b
Petr Zankov <peter@cyberhaven.io>
2016-12-04 21:06:50 +0100

    pump: use sleep mode

--- pump.py
+++ pump.py
@@ -13,6 +13,9 @@ class PumpInterface:
     # Pump properties
     max_rpm = 30
     step_angle = 1.8
+
+    # Driver properties
+    wake_up_time = 1e-3
     microsteps = 8
 
     # Calibration data
@@ -29,18 +32,26 @@ class PumpInterface:
 
         GPIO.setmode(GPIO.BCM)
 
+        GPIO.setup(settings.PUMP_GPIO_SLEEP, GPIO.OUT)
         GPIO.setup(settings.PUMP_GPIO_STEP, GPIO.OUT)
+
+        GPIO.output(settings.PUMP_GPIO_SLEEP, False)
         GPIO.output(settings.PUMP_GPIO_STEP, False)
 
     def __del__(self):
         GPIO.cleanup()
 
     def step(self, count):
+        GPIO.output(settings.PUMP_GPIO_SLEEP, True)
+        time.sleep(self.wake_up_time)
+
         for t in range(0, int(count)):
             GPIO.output(settings.PUMP_GPIO_STEP, True)
             time.sleep(0.5 / self.max_step_frequency)
             GPIO.output(settings.PUMP_GPIO_STEP, False)
             time.sleep(0.5 / self.max_step_frequency)
+
+        GPIO.output(settings.PUMP_GPIO_SLEEP, False)
 
     def pump(self, volume):
         self.step(volume * self.steps_per_litre)
@@ -48,7 +59,9 @@ class PumpInterface:
 
 def main():
     p = PumpInterface()
-    p.pump(3e-3)
+    while True:
+        p.pump(1e-3)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
