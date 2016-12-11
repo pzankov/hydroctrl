7e986e60fb354435029b660dc70ef9b0c41210aa
Petr Zankov <peter@cyberhaven.io>
2016-12-11 14:07:34 +0100

    controller: refactor NTP check loop

--- controller.py
+++ controller.py
@@ -19,17 +19,20 @@ def wait_for_ntp():
     """
 
     log('Waiting for NTP, press Ctrl+C to skip')
+
     try:
         while True:
             output = subprocess.check_output(['ntpq', '-pn'])
             lines = output.splitlines()[2:]  # skip header lines
-            for l in lines:
-                if l[0] == ord('*'):  # sync peer is labelled with a star
-                    return
+            sync_peers = sum(l[0] == ord('*') for l in lines)  # sync peer is labelled with a star
+            if sync_peers > 0:
+                break
             log('Waiting for NTP')
             time.sleep(5)
     except KeyboardInterrupt:
         log('NTP status check skipped')
+
+    log('NTP status OK')
 
 
 class Controller:
