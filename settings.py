5e0d97fa7b70571d95d7944bbf868607587f3308
Petr Zankov <peter@cyberhaven.io>
2017-01-09 22:44:28 +0100

    settings: calibrate pH sensor

--- settings.py
+++ settings.py
@@ -21,10 +21,10 @@ PH_ADC_I2C_ADDR = 0x4F
 PH_ADC_REF_V = 2.5
 
 PH_CALIBRATION = {
-    'temperature': 25,
+    'temperature': 21.2,
     'points': (
-        {'ph': 4.0, 'voltage': 1.427},
-        {'ph': 7.0, 'voltage': 1.250}
+        {'ph': 4.0, 'voltage': 1.418},
+        {'ph': 7.0, 'voltage': 1.224}
     )
 }
 
