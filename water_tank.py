3c9f31a020bdbc8f9feb835861be6c22bb49f090
Petr Zankov <peter@cyberhaven.io>
2017-03-26 18:10:23 +0200

    water_tank: add PressureInterface

--- water_tank.py
+++ water_tank.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from adc import ADS1115, ADCFilter
 from settings import UR, WATER_TANK_CONFIG
 
 
@@ -31,6 +32,31 @@ class LinearInterpolation:
         y2 = self.y[i2]
 
         return y1 + (x_new - x1) / (x2 - x1) * (y2 - y1)
+
+
+class PressureInterface:
+    """
+    MP3V5050DP pressure sensor interface.
+    """
+
+    # ADC data rate
+    adc_sps = 64
+
+    sensitivity = 54 * UR.mV / UR.kPa
+
+    def __init__(self, i2c_busn, i2c_addr, adc_channel, adc_fsr, pressure_offset):
+        self.adc = ADS1115(i2c_busn, i2c_addr)
+        self.adc.config(adc_channel, adc_fsr, self.adc_sps)
+        self.adc = ADCFilter(self.adc, samples_count=self.adc_sps)
+        self.pressure_offset = pressure_offset
+
+    def get_pressure_and_voltage(self):
+        voltage = self.adc.get_voltage()
+        pressure = voltage / self.sensitivity - self.pressure_offset
+        return pressure, voltage
+
+    def get_pressure(self):
+        return self.get_pressure_and_voltage()[0]
 
 
 class WaterTankInterface:
